e8fd13bc4bb2c90fc876597daf8c1239d3530d21
PapyRedstone <febvrealex@yahoo.fr>
2018-01-23 16:14:06 +0100

    nouveau fichier : Serveur/ressources/map/map1.lua

---
+++ Serveur/ressources/map/map1.lua
@@ -0,0 +1,20 @@
+mapSize = 5
+
+map = {
+   {0,0,0,0,0},
+   {0,0,0,0,0},
+   {0,0,0,0,0},
+   {0,0,0,0,0},
+   {0,0,0,0,0}
+}
+
+grass = 0
+water = 1
+
+--limited ressources then permanent ressources 
+wood = {2,3}
+bush = {4,5}
+stone = {6,7}
+cristal = {8,9} 
+iron = {10,11}
+gold = {12,13}
