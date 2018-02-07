70a4339ca79fd91f2a23d0e1c5a80b21435a221d
PapyRedstone <febvrealex@yahoo.fr>
2018-02-07 10:08:03 +0100

    modifié :         html/ECSEntity_8hpp.html 	modifié :         html/ECSEntity_8hpp_source.html 	modifié :         html/ECS_8hpp.html 	modifié :         html/ECS_8hpp_source.html 	modifié :         html/MoveSystem_8hpp_source.html 	modifié :         html/dir_d44c64559bbebec7f509842c48db8b23.html 	modifié :         html/files.html 	modifié :         include/ECS.hpp 	modifié :         include/ECSEntity.hpp

--- GameEngine/include/ECS.hpp
+++ GameEngine/include/ECS.hpp
@@ -1,6 +1,13 @@
 #ifndef ECS_HPP
 #define ECS_HPP
 
+/**
+ *\file ECS.hpp
+ *\author PapyRedstone
+ *\version 1.0
+ *\brief include tous les fichiers necessaire a l'utilisation de l'Entity Component System$
+ */
+
 #include "MoveSystem.hpp"
 
 #endif
--- GameEngine/include/ECSEntity.hpp
+++ GameEngine/include/ECSEntity.hpp
@@ -1,6 +1,13 @@
 #ifndef ECS_ENTITY_HPP
 #define ECS_ENTITY_HPP
 
+/**
+ *\file ECSEntity.hpp
+ *\author PapyRedstone
+ *\version 1.0
+ *\brief include tous les fichiers necessaire aux systemes
+ */
+
 #include "Component.hpp"
 #include "ComponentStorer.hpp"
 #include "EntityManager.hpp"
